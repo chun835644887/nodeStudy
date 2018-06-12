0ad38a05011d5af2aa229a7c194b80f3e0540313
chun835644887 <835644887@qq.com>
2018-06-13 00:06:38 +0800

    添加node的sql查询，reg页面注册的简单操作2018-06-13

--- js/nodeDevGuide/microblog/microblog/public/javascripts/reg-submit.js
+++ js/nodeDevGuide/microblog/microblog/public/javascripts/reg-submit.js
@@ -2,13 +2,17 @@ $("#reg-submit").click(function() {
 	var username = $("#username").val();
 	var passworld1 = $("#passworld1").val();
 	var passworld2 = $("#passworld2").val();
+	var sex = $("#sex").val();
+	var idCard = $("#idCard").val();
 	$.ajax({
 		type: "post",
 		url : "/reg",
 		data: {
 			username: username,
 			passworld1: passworld1,
-			passworld2: passworld2
+			passworld2: passworld2,
+			sex : sex,
+			idCard : idCard
 		},
 		success: function(data) {
 			console.log(data);
--- js/nodeDevGuide/microblog/microblog/routes/reg.js
+++ js/nodeDevGuide/microblog/microblog/routes/reg.js
@@ -1,12 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var query = require("../public/javascripts/mysql");
 
 /* GET home page. */
-
 module.exports = function(req, res, next){
 	var body = req.body;
-	console.log(`${JSON.stringify(req.body)}`)
-	if(body.username && username.passworld1 == username.passworld2){
-		res.redirect('/main/');
+	if(body.username 
+		&& body.passworld1 == body.passworld2 
+		&& body.sex
+		&& body.idCard){
+		console.log(body.username);
+		query.query("select * from user where username='" + body.username + "'",function(err, results, fields){
+			if(err){
+				return err;
+			}
+			if(!results.length){
+				var isertSql = "insert into user(username,sex,pwd,idcard) values(" 
+					+ "'" + body.username + "',"
+					+ "'" + body.sex + "',"
+					+ "'" + body.passworld1 + "',"
+					+ "'" + body.idCard + "'"
+					+ ")"
+					console.log(isertSql)
+				query.query(isertSql, function(err, results, fields){
+					console.log(123);
+					if(err){
+						console.log(err);
+						return err;
+					}
+					console.log(456);
+					console.log(results,fields);
+				});
+			}else{
+				// res.
+			}
+		});
+		// res.redirect('/main');
 	}
 };
